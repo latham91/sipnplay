17336d4e693ba6ce44cc876c34eb14e70b15de98
Aaron Latham <latham91@icloud.com>
2024-07-12 00:16:32 +0100

    enhancement: completed menu item card design

--- components/dashboard/MenuItemCard.jsx
+++ components/dashboard/MenuItemCard.jsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function MenuItemCard() {
   return (
     <div className="flex flex-col justify-between col-span-1 p-4 bg-white border rounded-lg shadow-md ">
-      <div>
-        <div className="pb-2 mb-2 border-b border-dashed">
+      <div className="flex flex-col gap-3">
+        <div className="pb-2 border-b border-dashed">
           <div className="flex items-center justify-between">
             <h3>The OG</h3>
             <span className="px-4 py-0.5 bg-sky-300 rounded-full text-white">Boba</span>
@@ -13,6 +13,7 @@ export default function MenuItemCard() {
             <p className="italic">Classic milk tea</p>
           </div>
         </div>
+
         <div>
           <h5>Prices</h5>
           <div className="flex items-center justify-between">
@@ -26,6 +27,14 @@ export default function MenuItemCard() {
           <div className="flex items-center justify-between">
             <p>Hot:</p>
             <p className="font-bold">$5.25</p>
+          </div>
+        </div>
+
+        <div>
+          <h5>Additionals</h5>
+          <div className="flex items-center justify-between">
+            <p>Extra Pearls:</p>
+            <p className="font-bold">$1.00</p>
           </div>
         </div>
       </div>

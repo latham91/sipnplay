121887743c7fe4281f0885bb487fcdb8b3209804
Mayranne <mayranne-@hotmail.com>
2024-07-11 19:34:34 +0100

    chore: created empty components to the homepage

--- app/(storefront)/page.jsx
+++ app/(storefront)/page.jsx
@@ -1,7 +1,17 @@
+import Hero from "@/components/storefront/Homepage/Hero";
+import About from "@/components/storefront/Homepage/About";
+import Events from "@/components/storefront/Homepage/Events";
+import Menu from "@/components/storefront/Homepage/Menu";
+import Reservations from "@/components/storefront/Homepage/Reservations";
+
 export default function StorefrontHomePage() {
   return (
     <div>
-      <h1>Homepage</h1>
+      <Hero />
+      <About />
+      <Events />
+      <Menu />
+      <Reservations />
     </div>
   );
 }

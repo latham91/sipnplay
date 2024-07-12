b0bf047a5420574d8444289489aa126bb038ca7c
Aaron Latham <latham91@icloud.com>
2024-07-12 10:40:07 +0100

    enhancement: created spin wheel component and added it to boardgame page, connect frontend with backend on the same page

--- components/storefront/Homepage/CardUpcomingEvent.jsx
+++ components/storefront/Homepage/CardUpcomingEvent.jsx
@@ -6,29 +6,25 @@ export default function CardUpcomingEvent({ event, long }) {
   return (
     <div className="relative">
       <Link
-        href={event.link}
-        className="relative z-10 group flex flex-col gap-4 hover:-rotate-3 my-transition bg-stone-50 backdrop-blur-sm border p-4"
+        href={"/"}
+        className="relative z-10 flex flex-col gap-4 p-4 border group hover:-rotate-3 my-transition bg-stone-50 backdrop-blur-sm"
       >
-        <div className="relative overflow-hidden h-56 w-full">
+        <div className="relative w-full h-56 overflow-hidden">
           <Image
             src={event.image}
             alt={event.title}
             width={200}
             height={200}
-            className="relative z-10 w-full h-full object-cover group-hover:scale-110 my-transition p-2"
+            className="relative z-10 object-cover w-full h-full p-2 group-hover:scale-110 my-transition"
           />
         </div>
-        <div className="p-2 flex flex-col gap-4">
-          <div className="bg-stone-800 text-white px-2 py-1">
-            <h4 className="text-stone-50 my-transition line-clamp-1">
-              🎯 {event.title}
-            </h4>
+        <div className="flex flex-col gap-4 p-2">
+          <div className="px-2 py-1 text-white bg-stone-800">
+            <h4 className="text-stone-50 my-transition line-clamp-1">🎯 {event.title}</h4>
           </div>
-          <p className={`text-stone-400 text-sm ${long ? "" : "line-clamp-3"}`}>
-            {event.description}
-          </p>
-          <div className="flex gap-2 items-center">
-            <p className=" font-bold line-clamp-2 group-hover:underline my-transition">
+          <p className={`text-stone-400 text-sm ${long ? "" : "line-clamp-3"}`}>{event.description}</p>
+          <div className="flex items-center gap-2">
+            <p className="font-bold  line-clamp-2 group-hover:underline my-transition">
               {long ? "Reserve your spot now" : "Read more"}
             </p>
             <ChevronRight className="size-4 -mb-0.5 my-transition" />
--- components/storefront/Homepage/GameRoulette.jsx
+++ components/storefront/Homepage/GameRoulette.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import CardCTA from "./CardCTA";
+import Image from "next/image";
 
 export default function GameRoulette() {
   // Bellow is the data I will need to fill out my card that I made as a component
@@ -10,19 +11,16 @@ export default function GameRoulette() {
     title: "🎯 A to Z Game List",
     description: (
       <>
-        Dive into our A to Z list of board games and uncover your next
-        obsession! Whether you're a fan of timeless classics or in search of
-        hidden gems, our curated collection has something for everyone.{" "}
+        Dive into our A to Z list of board games and uncover your next obsession! Whether you're a fan of timeless classics
+        or in search of hidden gems, our curated collection has something for everyone.{" "}
         <span className="font-bold text-stone-600">
-          From timeless classics that never go out of style to hidden gems
-          waiting to be discovered, there's something for everyone. 🥳
+          From timeless classics that never go out of style to hidden gems waiting to be discovered, there's something for
+          everyone. 🥳
         </span>
         <br />
-        <br /> Find the perfect game for your next gathering, be it a casual
-        get-together or a more competitive event.{" "}
+        <br /> Find the perfect game for your next gathering, be it a casual get-together or a more competitive event.{" "}
         <span className="font-bold text-stone-600">
-          Explore, play, and enjoy an unforgettable experience with friends and
-          family! 🎯
+          Explore, play, and enjoy an unforgettable experience with friends and family! 🎯
         </span>{" "}
         <br /> <br />
       </>
@@ -34,36 +32,39 @@ export default function GameRoulette() {
 
   return (
     <div className="relative z-20 bg-stone-50">
-      <div className="margin-x margin-y grid grid-cols-1 lg:grid-cols-2 gap-16">
+      <div className="grid grid-cols-1 gap-16 margin-x margin-y lg:grid-cols-2">
         {/* 🔻 Game Rouelette space - We will try to implement tomorrow */}
-        <div className="bg-pink-600 min-h-[400px]"></div>
+        <div className="bg-pink-600 min-h-[400px] relative">
+          <Image
+            src="/spinwheel-preview.png"
+            fill
+            className="object-cover object-center"
+            alt="Spinwheel Image"
+            draggable={false}
+          />
+        </div>
         {/* 🔻 Title + description + buttons */}
-        <div className="flex flex-col gap-16 items-start">
+        <div className="flex flex-col items-start gap-16">
           <div className="flex flex-col items-start">
-            <h1 className="relative z-20 bg-stone-900 text-stone-50 px-4 md:px-6 py-2 -mb-2 hover:scale-110 my-transition">
-              Let our{" "}
-              <span className="text-5xl max-md:text-4xl max-sm:text-3xl">
-                ✨
-              </span>
+            <h1 className="relative z-20 px-4 py-2 -mb-2 bg-stone-900 text-stone-50 md:px-6 hover:scale-110 my-transition">
+              Let our <span className="text-5xl max-md:text-4xl max-sm:text-3xl">✨</span>
             </h1>
             <h1 className="relative z-10 bg-[#dde9d3] px-4 md:px-6 py-2 -mb-2 hover:scale-110 my-transition">
               Game Roulette
             </h1>
-            <h1 className="relative z-0 bg-stone-900 text-stone-50 px-4 md:px-6 py-2 -mb-2 hover:scale-110 my-transition">
+            <h1 className="relative z-0 px-4 py-2 -mb-2 bg-stone-900 text-stone-50 md:px-6 hover:scale-110 my-transition">
               decide for you!
             </h1>
           </div>
           <p className="">
             <span className="font-bold text-stone-600">
-              Welcome to Game Roulette, the perfect way to choose from our
-              collection of over 300 board games.
+              Welcome to Game Roulette, the perfect way to choose from our collection of over 300 board games.
             </span>{" "}
             Simply click the button below to access this amazing feature! 🤩
             <br />
             <br />
-            Press "Play" and let the wheel decide your next adventure. Ask our
-            friendly staff for the game and enjoy a fresh, fun experience with
-            your friends! 🎲
+            Press "Play" and let the wheel decide your next adventure. Ask our friendly staff for the game and enjoy a fresh,
+            fun experience with your friends! 🎲
           </p>
           <div className="flex gap-3">
             <Button variant="default" asChild>
